fd76a1a929e6aa25779daa927787ffb7898f1278
iair-embon <iair.embon@gmail.com>
2022-07-27 17:27:16 -0300

    nc

--- Figures/Code/Fig_3a.R
+++ Figures/Code/Fig_3a.R
@@ -15,6 +15,36 @@ load(file= filepath)
 source(root$find_file("Analysis/AuxiliaryFunctions/DataFrame_subset.R"))
 d <- DataFrame_subset(df_total)
 
+d.sorted <- d %>% 
+  select(mc,PC,Participant) %>% 
+  arrange(mc) %>%
+  mutate(nr = row_number()) %>%
+  pivot_longer(!c(Participant, nr))
+
+ggplot(d.sorted, aes(x= unique(nr) ,
+                      y=value,color=name)) +
+  geom_point(size = 2) +
+  scale_color_manual(values = c("black","grey"),labels = c("Metacognition", "Performance"))+
+  scale_x_continuous(expand = expansion(mult = c(0.01, 0.01))) +
+  scale_y_continuous(expand = expansion(mult = c(0.01, 0.1))) +
+  theme(axis.line = element_line(colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        plot.margin = margin(1, 1,1, 1, "cm"),
+        legend.text =  element_text(size = 25),
+        legend.position = c(0.7, 0.2),
+        legend.background = element_blank(),
+        legend.key = element_blank(),
+        legend.title = element_blank(),
+        panel.background = element_blank(),
+        axis.text.x = element_text(size = 30),
+        axis.text.y = element_text(size = 30),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank())
+
+##########
+
 mc.sorted <-  d[order(d$mc),]
 subjects <- 1:nrow(mc.sorted)
 mc.sorted$s <- subjects
