e934eae3b051b8688c8139227fd1231259b99afc
iair-embon <iair.embon@gmail.com>
2022-11-03 15:16:33 -0300

    nc

--- Analysis/RegressionAnalysis/MixedLogisticRegressionAnalysis.R
+++ Analysis/RegressionAnalysis/MixedLogisticRegressionAnalysis.R
@@ -12,12 +12,12 @@ filepath <- root$find_file("Data/df_total_filtered.Rda")
 load(file= filepath)
 
 # preprocessing
-df_total$gender <- ifelse(df_total$gender == "Masculino",1,0)
-df_total$discrimination_is_correct <- ifelse(df_total$discrimination_is_correct == TRUE, 1, 0)
-df_total$Participant <- factor(df_total$Participant)
-df_total$confidence_key.norm <- (df_total$confidence_key - 1) / 3 
-df_total$AQ_test.std <-  (df_total$AQ_test - mean(df_total$AQ_test)) / sd(df_total$AQ_test)
-df_total$age.std <- (df_total$age - mean(df_total$age))/sd(df_total$age)
+df_total_filtered$gender <- ifelse(df_total_filtered$gender == "Masculino",1,0)
+df_total_filtered$discrimination_is_correct <- ifelse(df_total_filtered$discrimination_is_correct == TRUE, 1, 0)
+df_total_filtered$Participant <- factor(df_total_filtered$Participant)
+df_total_filtered$confidence_key.norm <- (df_total_filtered$confidence_key - 1) / 3 
+df_total_filtered$AQ_test.std <-  (df_total_filtered$AQ_test - mean(df_total_filtered$AQ_test)) / sd(df_total_filtered$AQ_test)
+df_total_filtered$age.std <- (df_total_filtered$age - mean(df_total_filtered$age))/sd(df_total_filtered$age)
 
 a_log <- glmer(discrimination_is_correct ~ confidence_key.norm +
                  confidence_key.norm:AQ_test.std +
@@ -26,7 +26,7 @@ a_log <- glmer(discrimination_is_correct ~ confidence_key.norm +
                  confidence_key.norm:AQ_test.std:gender +
                  confidence_key.norm:AQ_test.std:age.std +
                  (1|Participant),
-               data = df_total,
+               data = df_total_filtered,
                family = binomial,
                control=glmerControl(optimizer="bobyqa",
                                     optCtrl=list(maxfun=2e5)))
@@ -39,7 +39,7 @@ save(a_log, file = "Data/Regression_Results/MixedLogisticRegressionAnalysis.RDat
 a_log2 <- glmer(discrimination_is_correct ~ confidence_key.norm +
                  confidence_key.norm:AQ_test.std +
                  (1|Participant),
-               data = df_total,
+               data = df_total_filtered,
                family = binomial,
                control=glmerControl(optimizer="bobyqa",
                                     optCtrl=list(maxfun=2e5)))
